264c2e5ddde19757bc6ba1a48208f7046ca485c6
conghaonet <hao.cong@app2m.com>
2019-11-09 15:59:27 +0800

    代码优化

--- src/com/app2m/java/practice/algorithm/MaoPao.java
+++ src/com/app2m/java/practice/algorithm/MaoPao.java
@@ -9,6 +9,7 @@ public class MaoPao {
 	public static void main(String[] args) {
 		int array[] = {-7, 4, 2, 9, -1, 7, 6, 0, 5, 3};
 		sortSmall2Big(array);
+		System.out.print("Result: ");
 		for(int i:array) {
 			System.out.print(i+",");
 		}
@@ -21,7 +22,7 @@ public class MaoPao {
 	public static void sortSmall2Big(int[] array) {
 		for (int i=0; i<array.length; i++) {
 			for(int j=i+1; j<array.length; j++) {
-				if(array[i] >array[j]) {
+				if(array[i] > array[j]) {
 					int temp = array[i];
 					array[i] = array[j];
 					array[j] = temp;
